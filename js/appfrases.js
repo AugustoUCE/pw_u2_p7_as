0b435c745426b2969a22ebbee9d4cd8a4e27690c
AugustoUCE <aysalazar@uce.edu.ec>
2024-12-05 19:42:50 -0500

    taller15

--- js/appfrases.js
+++ js/appfrases.js
@@ -1,24 +1,72 @@
-const frases = [    
-    {frase:'El que madruga, Dios lo ayuda', autor:'Pepito perez'},
-    {frase:'El que mucho avanza', autor:'Juan paez'},
-    {frase:'texto 1', autor:'tito mindo'},
-    {frase:'La patria es de todos', autor:'Otto perez'},
-    {frase:'Vacuna no', autor:'Pedro rez'},
-    {frase:'texto jorge', autor:'Jorge tata'},
-    {frase:'confirma texto andrea', autor:'Andrea Alban'},
-    
-
-]
-
+const frases = [
+  { frase: "El que madruga, Dios lo ayuda", autor: "Pepito perez" },
+  { frase: "El que mucho avanza", autor: "Juan paez" },
+  { frase: "texto 1", autor: "tito mindo" },
+  { frase: "La patria es de todos", autor: "Otto perez" },
+  { frase: "Vacuna no", autor: "Pedro rez" },
+  { frase: "texto jorge", autor: "Jorge tata" },
+  { frase: "confirma texto andrea", autor: "Andrea Alban" },
+];
 const app = Vue.createApp({
-    methods: {
-        
-    },
-    data() {
-        return {
-            listaFrase: frases,
-        }
-    },
+  methods: {
+    agregarFrase() {
+      console.log(this.frase);
+      console.log(this.autor);
+      const nuevaFrase = {
+        frase: this.frase,
+        autor: this.autor,
+      };
 
+      this.listafrases.unshift(nuevaFrase);
+    },
+    agregarFraseFinal() {
+      console.log(this.frase);
+      console.log(this.autor);
+      const nuevaFrase = {
+        frase: this.frase,
+        autor: this.autor,
+      };
+
+      this.listafrases.push(nuevaFrase);
+    },
+    //desectructuracion de event con { }
+    //para evitar escribir mucho event.target asi sucesivamente
+    //console.log(event.charCode);
+    eventoKeyPress({ charCode, cancelable, key,keyCode, target }) {
+      if (key === "Enter") {
+        //console.log("Evento");
+        console.log(charCode);
+        console.log(cancelable);
+        console.log(key);
+        console.log(keyCode);
+        console.log(target.baseURI);
+        this.agregarFraseFinal();
+      }
+
+      //la ruta completa de un event es event.target.value
+      //por que el objeto event es un objeto de javascript
+    },
+    eventoKeyPressModificador() {
+        if (key === "Enter") {
+        console.log("Evento");
+        console.log(charCode);
+        console.log(cancelable);
+        console.log(key);
+        console.log(keyCode);
+        console.log(target.baseURI);
+        this.agregarFraseFinal();
+        }
+
+    },
+  },
+  data() {
+    return {
+      listafrases: frases,
+      frase: null,
+      //autor:'sin autor'
+      autor: null,
+    };
+  },
 });
-app.mount("#myapp");
+
+app.mount("#myApp");
